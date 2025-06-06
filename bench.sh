b383d38ce60c05ae15cc73a94aca44e8563a42ec
bibica <119997727+bibicadotnet@users.noreply.github.com>
2025-06-07 04:10:34 +0700

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -76,18 +76,17 @@ speed_test() {
 speed() {
     speed_test '' 'Speedtest.net'
     speed_test '68864' 'San Jose, US'
-    speed_test '24215' 'Paris, FR'
+    speed_test '62493' 'Orange France, FR'
     speed_test '28922' 'Amsterdam, NL'
-    speed_test '32155' 'Hongkong, CN'
-    speed_test '6527' 'Seoul, KR'
+    speed_test '13538' 'Hong Kong, CN'
     speed_test '7311' 'Singapore, SG'
-    speed_test '21569' 'Tokyo, JP'
-    speed_test '2515' 'FPT-HCM, VN'
-    speed_test '2552' 'FPT-HN, VN'
-    speed_test '17758' 'VNPT-HCM, VN'
-    speed_test '17757' 'VNPT-HN, VN'
-    speed_test '54812' 'Viettel-HCM, VN'
-    speed_test '59915' 'Viettel-DN, VN'
+    speed_test '50467' 'Tokyo, JP'
+    speed_test '2515' 'FPT HCM, VN'
+    speed_test '2552' 'FPT HN, VN'
+    speed_test '17758' 'VNPT HCM, VN'
+    speed_test '17757' 'VNPT HN, VN'
+    speed_test '54812' 'Viettel HCM, VN'
+    speed_test '59915' 'Viettel DN, VN'
 }
 
 io_test() {
